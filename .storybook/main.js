4c61d8f65d98b9b1aeb4eed48f5fbd594c3b35c7
Jordan Barbosa <jordansbarbosa01@gmail.com>
2020-05-18 01:01:04 -0400

    Configured storybook

---
+++ .storybook/main.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const merge = require('webpack-merge');
+
+module.exports = {
+  stories: ['../src/components/**/*.stories.(ts|tsx|js|jsx)'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    'storybook-addon-jsx/register',
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-viewport/register',
+    '@storybook/addon-storysource',
+    '@storybook/addon-backgrounds/register',
+    '@storybook/addon-a11y/register',
+    'storybook-formik/register',
+  ],
+  webpackFinal: async config =>
+    merge.smart(config, {
+      module: {
+        rules: [
+          {
+            test: /\.(ts|tsx)$/,
+            loader: require.resolve('babel-loader'),
+            options: {
+              presets: [['react-app', { flow: false, typescript: true }]],
+            },
+          },
+          {
+            test: /\.stories\.tsx?$/,
+            loaders: [
+              {
+                loader: require.resolve('@storybook/source-loader'),
+                options: { parser: 'typescript' },
+              },
+            ],
+            enforce: 'pre',
+          },
+        ],
+      },
+      resolve: {
+        extensions: ['.ts', '.tsx'],
+        alias: {
+          '@/analytics': path.join(__dirname, '../src/utils/analytics'),
+          '@/interfaces/*': path.join(__dirname, '../typescript/interfaces/*'),
+          '@/components/*': path.join(__dirname, '../src/components/*'),
+          '@/shared/*': path.join(__dirname, '../src/components/Shared/*'),
+          '@/config/*': path.join(__dirname, '../src/config/*'),
+          '@/redux/*': path.join(__dirname, '../src/redux/*'),
+          '@/utils/*': path.join(__dirname, '../src/utils/*'),
+          '@/stories/*': path.join(__dirname, '../src/stories/*'),
+        },
+      },
+    }),
+};
