e2cccb2fdb34edfc3948be97ef554efd294fd631
Jordan Barbosa <jordansbarbosa01@gmail.com>
2020-05-18 01:03:44 -0400

    Commented out for now

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -8,7 +8,7 @@ import Col from 'react-bootstrap/Col';
 import { useSelector, useDispatch } from 'react-redux';
 import Axios from 'axios';
 // import { setProjectsLoading, setProjectsList, setProjectsError } from '../../redux/ActionCreators';
-import Project from '../typescript/interfaces/Project.interface';
+import Project from '@/interfaces/Project.interface';
 import Loader from '../src/components/Shared/Loader/Loader';
 import Head from 'next/head';
 
@@ -30,28 +30,28 @@ export default function Projects() {
     // });
   }, []);
 
-  const loadProjects = () => {
-    if (!loading && list.length && !error) {
-      return list.map((project: Project) => {
-        // return <ProjectItem key={project._id} project={project} />;
-        return 'a';
-      });
-    } else if (loading && !list.length && !error) {
-      return <Loader />;
-    } else if (!loading && !list.length && error) {
-      return 'Error...';
-    } else if (!loading && !list.length && !error) {
-      return (
-        <div className="no-projects d-flex justify-content-center align-items-center">
-          <h1>
-            NO PROJECTS <i className="far fa-frown"></i>
-          </h1>
-        </div>
-      );
-    } else {
-      return <Loader />;
-    }
-  };
+  // const loadProjects = () => {
+  //   if (!loading && list.length && !error) {
+  //     return list.map((project: Project) => {
+  //       // return <ProjectItem key={project._id} project={project} />;
+  //       return 'a';
+  //     });
+  //   } else if (loading && !list.length && !error) {
+  //     return <Loader />;
+  //   } else if (!loading && !list.length && error) {
+  //     return 'Error...';
+  //   } else if (!loading && !list.length && !error) {
+  //     return (
+  //       <div className="no-projects d-flex justify-content-center align-items-center">
+  //         <h1>
+  //           NO PROJECTS <i className="far fa-frown"></i>
+  //         </h1>
+  //       </div>
+  //     );
+  //   } else {
+  //     return <Loader />;
+  //   }
+  // };
 
   return (
     <>
