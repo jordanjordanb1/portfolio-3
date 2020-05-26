55316a8f027d84e1e6ed49e747207abcd11e4250
Jordan Barbosa <jordansbarbosa01@gmail.com>
2020-05-25 23:44:22 -0400

    Updated some packages

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,7 +7,6 @@ module.exports = {
     '@storybook/addon-actions',
     '@storybook/addon-links',
     'storybook-addon-jsx/register',
-    '@storybook/preset-create-react-app',
     '@storybook/addon-knobs/register',
     '@storybook/addon-viewport/register',
     '@storybook/addon-storysource',
